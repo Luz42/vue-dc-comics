0329ca6b9b24246fab9b58933222f91d6133ec8c
Luz42 <a.evangelista93@gmail.com>
2022-09-22 14:39:28 +0200

    STEP 1
    inizializzato il nuovo progetto vue-dc-comics
    
    STEP 2
    
    inizializzata la pagina e rimosso il contenitore standard
    
    STEP 3
    
    creato un nuovo contenitore e collegato
    
    headerComponent

---
+++ vue-dc-comics/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    Questo è
+    <headerComponent/>
+  </div>
+</template>
+
+<script>
+import headerComponent from './components/headerComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+    headerComponent
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ vue-dc-comics/src/components/headerComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Il mio primo componente</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'headerComponent'
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+    
+  </style>
+  
